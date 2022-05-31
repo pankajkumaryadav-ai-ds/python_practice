7a3475f06bc798b7bccc11687226e5a29ae90849
Pankaj Kumar Yadav <85467416+pankajkumaryadav-ai-ds@users.noreply.github.com>
2022-05-31 16:08:59 +0530

    Add files via upload

---
+++ 03_typecasting.py
@@ -0,0 +1,10 @@
+a="123456"
+a = int(a)
+# print(type(a))
+print(a+5)
+# integer to string conversion
+str(31)
+# string to integer converion
+int("32")
+# integer to float conversion
+float(32)
---
+++ ch_02_practice.py
@@ -0,0 +1,36 @@
+# add two numbers
+a=34
+b=5
+print("the sum of a and b is",a+b)
+
+# 2nd problem
+a=45
+b=15
+print("the remainder when a divides b is ", a%b)
+# % is used to find the remainder
+
+# 3rd problem
+# immediate mode REPL = python+enter in cmd prompt
+a = input("Entrer a number: ")
+print(type(a))
+# to convert it to int 
+a=int(a)
+print(type(a))
+
+# 4th problem
+a=45
+b=34
+print(a>b)
+
+# 5th problem average of two numbers from user
+a=input("Enter first number:")
+b=input("Enter second number:")
+a= int(a)
+b= int(b)
+avg=(a+b)/2
+print("the average of and b is", avg)
+
+# 6th problem calculate square of a number from user
+a=input("Enter a number:")
+a=int(a)
+print("The square of the given number is", a*a)
