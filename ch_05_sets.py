56102b3b8d246b003bdeb911b77c44b24232cb70
Pankaj Kumar Yadav <85467416+pankajkumaryadav-ai-ds@users.noreply.github.com>
2022-06-02 16:29:46 +0530

    Add files via upload

---
+++ ch_05_sets.py
@@ -0,0 +1,14 @@
+# sets are unordered
+# sets are unindexed cannot access through indexes
+# there is no way of changing the set values
+a={1,1,2,3}
+print(a)
+print(type(a))
+# sets should not allow repetative values or duplicative values
+
+# empty set
+a={}
+print(type(a)) #this syntax will create an empty dictionary but not empty set
+# an empty set can be created by 
+a=set()
+print(type(a))
