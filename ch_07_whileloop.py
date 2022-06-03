19e72a56e8d956ed45d65e84d31e83ef986d044a
Pankaj Kumar Yadav <85467416+pankajkumaryadav-ai-ds@users.noreply.github.com>
2022-06-03 16:35:12 +0530

    Add files via upload

---
+++ ch_07_whileloop.py
@@ -0,0 +1,31 @@
+i=0
+while(i<10):
+    print("yes " + str(i))
+    i=i+1
+print("done")
+
+# printing 50 numbers
+i=0
+while(i<51):
+    print(str(i))
+    i=i+1 #this prints from 0-50 numbers
+print("Done printing 50 numbers")
+
+i=0
+while(i<51):
+    i=i+1 #this prints from 1-51 numbers
+    print(str(i))
+print("Done printing 50 numbers")
+
+i=0
+while(i<=2):
+    print("Pankaj")
+    i=i+1
+print("Succesfully completed")
+
+# print content in the list using list
+fruit=["Banana","watermelon","mangoes"]
+i=0
+while(i<len(fruit)):
+    print(fruit)
+    i=i+1
