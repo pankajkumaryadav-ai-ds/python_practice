405ab59a39f8bde642fcaeb70d6ea1e072d0feb6
Pankaj Kumar Yadav <85467416+pankajkumaryadav-ai-ds@users.noreply.github.com>
2022-06-06 10:12:21 +0530

    Add files via upload

---
+++ playing_sound.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('C:\\Users\\panka\\Music\\yt1s.com - Behti Hawa Sa Tha Woh  3 Idiots  Aamir Khan Madhavan Sharman J  Shaan  Shantanu M  Swanand K\\play.mp3')
